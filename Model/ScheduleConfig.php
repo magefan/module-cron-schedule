ea57531c1441665bd7a86ed8e60c0827309decf2
ivan.hrytsai <ivan.hrytsai@magefan.com>
2024-09-09 17:43:46 +0300

    group for Schedule Log and datetime filter

---
+++ Model/ScheduleConfig.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ */
+
+declare(strict_types=1);
+
+namespace Magefan\CronSchedule\Model;
+
+use Magento\Cron\Model\Config as CronConfig;
+
+class ScheduleConfig
+{
+    /**
+     * @var CronConfig
+     */
+    private $cronConfig;
+
+    /**
+     * @param CronConfig $cronConfig
+     */
+    public function __construct(
+        CronConfig $cronConfig
+    )
+    {
+        $this->cronConfig = $cronConfig;
+    }
+
+    /**
+     * @param $jobCode
+     * @return int|string|null
+     */
+    public function getCronJobGroup($jobCode)
+    {
+        $cronJobs = $this->cronConfig->getJobs();
+
+        foreach ($cronJobs as $group => $groupConfig) {
+
+            if (array_key_exists($jobCode,$groupConfig)) {
+                return $group;
+            }
+        }
+        return null;
+    }
+}
---
+++ Setup/Patch/Data/SetGroupForAllCronSchedule.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ */
+
+declare(strict_types=1);
+
+namespace Magefan\CronSchedule\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magefan\CronSchedule\Model\ScheduleConfig;
+
+class SetGroupForAllCronSchedule implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var ScheduleConfig
+     */
+    private $scheduleConfig;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param ScheduleConfig $scheduleConfig
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        ScheduleConfig $scheduleConfig
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->scheduleConfig = $scheduleConfig;
+    }
+
+    /**
+     * @return array|string[]
+     */
+    public static function getDependencies()
+    {
+        return [
+        ];
+    }
+
+    /**
+     * @return array|string[]
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * @return void
+     */
+    public function apply()
+    {
+        $connection = $this->moduleDataSetup->getConnection();
+        $tableName = $this->moduleDataSetup->getTable('cron_schedule');
+
+        $connection->startSetup();
+
+        $select = $connection->select()
+            ->from($tableName, ['job_code']);
+        $jobCodes = $connection->fetchCol($select);
+
+        foreach ($jobCodes as $jobCode) {
+            $group = $this->scheduleConfig->getCronJobGroup($jobCode);
+
+            $connection->update(
+                $tableName,
+                ['group' => $group],
+                ['job_code = ?' => $jobCode]
+            );
+        }
+        $connection->endSetup();
+    }
+}
